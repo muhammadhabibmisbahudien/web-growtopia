aec0ab9b5515043c49ee7f44e1bb80eb61001358
mfatihrabbani <mfatihrabbani@gmail.com>
2024-07-06 22:43:11 +0700

    feat: updaet

--- src/pages/bot/CardBot.tsx
+++ src/pages/bot/CardBot.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import {
     Select,
@@ -10,11 +10,12 @@ import {
   } from "@/components/ui/select"
 
 import { Input } from "@/components/ui/input";
+import { FormDescription } from "@/components/ui/form";
   
 
 export default function CardBot() {
     return (
-        <div className="flex justify-center mt-10">
+        <div className="flex justify-center my-10">
             <Card className="w-1/3 py-10">
                 <CardContent className="flex flex-col w-full justify-center">
                         <div className="flex justify-center">
@@ -33,7 +34,7 @@ export default function CardBot() {
                                         <SelectValue placeholder="Type CID" />
                                     </SelectTrigger>
                                     <SelectContent>
-                                        <SelectItem value="light">Level 1</SelectItem>
+                                        <SelectItem value="LEVEL_1">Level 1</SelectItem>
                                         <SelectItem value="dark">Level 5</SelectItem>
                                         <SelectItem value="system">Level 10</SelectItem>
                                     </SelectContent>
@@ -43,10 +44,33 @@ export default function CardBot() {
                                     <Label>Amount</Label>
                                     <Input />
                                 </div>
-
                             </div>
                             <div>
-                                <Button>Buy</Button>
+                                <Card>
+                                    <CardHeader>
+                                        <h1 className="font-bold text-3xl">Payment Method</h1>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <div className="mb-10">
+                                            <div className="mb-2">
+                                                <h1 className="text-2xl">World Depo : <b>DEPOHABIB</b></h1>
+                                                <h1 className="text-2xl">Bot Depo   : <b>BOTDEPOHABIB</b></h1>
+                                            </div>
+                                            <h1 className="">Your Balance : 3 WL</h1>
+                                        </div>
+                                        <div className="space-y-4">
+                                            <Label>Set Your GrowId</Label>
+                                            <div>
+                                                <Input />
+                                                <h4 className="text-sm">Your GrowId : Habib</h4>
+                                            </div>
+                                            <Button>Save Changes</Button>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                            <div >
+                                <Button className="w-full">Buy</Button>
                             </div>
                         </div>
                 </CardContent>
